775bff0fe3958fc63a3f1c903108f0316796957d
Dikov Sergey <dikovsa@mail.ru>
2023-11-30 15:05:42 +0300

    add code

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,3 +1,4 @@
+import os.path
 from datetime import datetime
 
 from selene import browser, have, be
@@ -35,12 +36,11 @@ def test_fill_practice_form():
     browser.all('.react-datepicker__month-select>option').element_by(have.exact_text('June')).click()
     browser.all('.react-datepicker__year-select>option').element_by(have.exact_text('1989')).click()
     browser.all('*.react-datepicker__day').element_by(have.text('15')).click()
-    # browser.element('#dateOfBirthInput').should(have.value('15 Jun 1989'))
+    browser.element('#dateOfBirthInput').should(have.value('15 Jun 1989'))
 
     browser.element('#subjects-label').should(have.exact_text('Subjects'))
     browser.element('#subjectsInput').should(be.blank)
-    browser.element('#subjectsInput').type('Python').press_enter()
-    # browser.element('#subjectsInput').should(have.value('Python'))
+    browser.element('#subjectsInput').type('Computer Science').press_enter()
 
     browser.element('[for="hobbies-checkbox-1"]').should(have.exact_text('Sports'))
     browser.element('[for="hobbies-checkbox-2"]').should(have.exact_text('Reading'))
@@ -48,18 +48,35 @@ def test_fill_practice_form():
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('[for="hobbies-checkbox-2"]').click()
 
+    browser.element('[for="uploadPicture"]').should(have.exact_text('Select picture'))
+    browser.element('#uploadPicture').send_keys(os.path.abspath('./img/test.jpg'))
+
     browser.element('#currentAddress-label').should(have.exact_text('Current Address'))
     browser.element('#currentAddress').should(be.blank)
     browser.element('#currentAddress').should(have.attribute('placeholder').value('Current Address'))
     browser.element('#currentAddress').type('Moscow')
-    # browser.element('#currentAddress').should(have.value('Moscow'))
-
-
-def test_temp():
-    browser.open('/')
+    browser.element('#currentAddress').should(have.value('Moscow'))
 
     browser.element('#stateCity-label').should(have.exact_text('State and City'))
-    browser.element('[class=" css-1wa3eu0-placeholder"]').should(have.exact_text('Select State'))
+    browser.element('#state > div > div.css-1hwfws3 > div.css-1wa3eu0-placeholder').should(
+        have.exact_text('Select State'))
+    browser.element('#state > div > div.css-1wy0on6').click()
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#city > div > div.css-1hwfws3 > div.css-1wa3eu0-placeholder').should(
+        have.exact_text('Select City'))
+    browser.element('#react-select-4-input').type('Noida').press_enter()
 
+    browser.element('#submit').press_enter()
 
-...
+    browser.element('.table-bordered').all('td').even.should(have.exact_texts(
+'Sergey Dikov',
+        'dikovsa@mail.ru',
+        'Male',
+        '9101234567',
+        '15 June,1989',
+        'Computer Science',
+        'Sports, Reading',
+        'test.jpg',
+        'Moscow',
+        'NCR Noida'))
+
