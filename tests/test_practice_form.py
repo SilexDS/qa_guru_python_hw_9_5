b0c4471a78f66e9f70fa95ee5603d19e089ee0c1
Viktoria <48157182+violin88@users.noreply.github.com>
2024-03-07 21:03:10 +0400

    изменила реализацию

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -11,27 +11,30 @@ def test_fill_practice_form():
     '''Добавление в корзину через каталог'''
 
     browser.element('[data-test-id="button__catalog"]').click() #поиск по каталогу
-    browser.all('//li[contains(@class, "parent-category")')[11].locate().text # не получилось сделать по CSS. НЕ ищется по номеру элемента
-    browser.all('// *[contains(@class , "filter-options__parameter")][236]').click()
+    time.sleep(5)
 
-    browser.element() # добавить скролл
+    browser.element('[id="filters"]>[href="/category/khobbi-i-tvorchestvo-10008"]').click() #  поиск в каталоге по "Детские товары"
 
-    browser.all('[data-test-id="text__product-name"]')[0].should(
-        have.text('Конструктор LEGO Architecture, 21054, "Белый дом"')).click()
-    browser.element('[data-test-id="button__add-cart"]').click()
-    browser.element('[class="cart-enter"]').click()
+   # browser.element('[id="filters"] [data-test-id="checkbox__filter"]')[237].click() # поиск по бренду LEGO
 
-
-    ''' Добавление в корзину через поиск'''
-    browser.element('[class="default-input"]').type('Конструктор LEGO Architecture').press_enter()
-    browser.all('[data-test-id="text__product-name"]')[0].should(
-        have.text('Конструктор LEGO Architecture, 21054, "Белый дом"')).click()
-    browser.element('[data-test-id="button__add-cart"]').click()
-    browser.element('[class="cart-enter"]').click()
-
-    '''Добавление в корзину через страницу избранное'''
-    browser.element('class="ui-link action-button"').click()
-    browser.element('[data-test-id="button__add-to-cart" ]').click()
+    # browser.element('').perform(command.js.scroll_into_view).should(be.visible) # добавить скролл
+    #
+    # browser.all('[data-test-id="text__product-name"]')[0].should(
+    #     have.text('Конструктор LEGO Architecture, 21054, "Белый дом"')).click()
+    # browser.element('[data-test-id="button__add-cart"]').click()
+    # browser.element('[class="cart-enter"]').click()
+    #
+    #
+    # ''' Добавление в корзину через поиск'''
+    # browser.element('[class="default-input"]').type('Конструктор LEGO Architecture').press_enter()
+    # browser.all('[data-test-id="text__product-name"]')[0].should(
+    #     have.text('Конструктор LEGO Architecture, 21054, "Белый дом"')).click()
+    # browser.element('[data-test-id="button__add-cart"]').click()
+    # browser.element('[class="cart-enter"]').click()
+    #
+    # '''Добавление в корзину через страницу избранное'''
+    # browser.element('class="ui-link action-button"').click()
+    # browser.element('[data-test-id="button__add-to-cart" ]').click()
 
 
 
